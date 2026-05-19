99c05b0b07ecdbac6dff763ee8d90f2e3f0d2115
imrezao <m.r.omidi484@gmail.com>
2026-05-19 20:23:09 +0330

    add more tests

--- src/main.py
+++ src/main.py
@@ -14,11 +14,19 @@ def happy_num(n: int):
     while n != 1 and (n not in seen_num):
         seen_num.add(n)
         n = sum(int(i)**2 for i in str(n))
-        # print(n)
-
+        
     return n == 1    
 
 
 if __name__ == '__main__':
-    assert happy_num(19)  is True
+    assert happy_num(7) is True
+    assert happy_num(10) is True
+    assert happy_num(13) is True
+    assert happy_num(19) is True
+
+    assert happy_num(4) is False
+    assert happy_num(16) is False
+    assert happy_num(20) is False
     assert happy_num(123) is False
+
+    print('successfully!')
